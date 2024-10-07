81d517a4b32e43cbcf10ad55679bcac4225b810d
qsz78547 <qsz78547@naver.com>
2024-10-07 12:06:46 +0900

    first commit

---
+++ src/main/java/com/hello/member/Member.java
@@ -0,0 +1,20 @@
+package com.hello.member;
+
+
+import lombok.Data;
+
+@Data
+public class Member {
+
+    private String memberId;
+    private String name;
+
+    public Member() {
+
+    }
+
+    public Member(String memberId, String name) {
+        this.memberId = memberId;
+        this.name = name;
+    }
+}
